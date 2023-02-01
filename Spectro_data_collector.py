2b4356620ef8ed2e1557039e97ed42d7225ca6e6
Monish <monishkumar.vr@gmail.com>
2023-02-01 22:12:54 +0530

    grid size fix

--- Spectro_data_collector.py
+++ Spectro_data_collector.py
@@ -2,6 +2,8 @@ import streamlit as st
 from st_aggrid import AgGrid, GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 import pandas as pd
+from agstyler import draw_grid
+
 
 sheet_id = "1-SJVJmE85vHo8EUP1MBDmrK-pK7dSAdfnsByKrfoCRE"
 sheet_name = "Items"
@@ -11,6 +13,8 @@ df = pd.read_csv(url)
 df.drop(['Yield', 'Price', 'Size'], axis = 1, inplace = True)
 df.fillna(0, inplace = True)
 
+st.set_page_config(page_title="My dashboard", layout="wide", initial_sidebar_state="expanded")
+
 f_name = st.text_input("Enter the foundry name")
 #st.dataframe(data=df2, width=None, height=None)
 
@@ -19,8 +23,39 @@ gd.configure_selection(selection_mode='multiple', use_checkbox=True)
 gd.configure_default_column(editable = True, groupable = True)
 gridoptions = gd.build()
 
+formatter = {
+    'Item Name': ('Item Name', {'width': 80}),
+    'Type': ('Type', {'width': 50}),
+    # 'Metal RR': ('Metal RR', {'width': 40}),
+    # 'Min': ('Min', {'width': 30}),
+    # 'Max': ('Max', {'width': 30}),
+    'C': ('C', {'width': 30}),
+    'Si': ('Si', {'width': 30}),
+    'Mn': ('Mn', {'width': 30}),
+    'S': ('S', {'width': 30}),
+    'P': ('P', {'width': 30}),
+    'Mg': ('Mg', {'width': 30}),
+    'Cr': ('Cr', {'width': 30}),
+    'Cu': ('Cu', {'width': 30}),
+    'Ni': ('Ni', {'width': 30}),
+    'Al': ('Al', {'width': 30}),
+    'Ti': ('Ti', {'width': 30}),
+    'Mo': ('Mo', {'width': 30}),
+}
+
 st.write('## Items Selection list')
-grid_table = AgGrid(df, height=250, gridOptions=gridoptions, update_mode=GridUpdateMode.SELECTION_CHANGED)
+grid_table = draw_grid(
+    df,
+    formatter=formatter,
+    fit_columns=True,
+    selection='multiple',  # or 'single', or None
+    use_checkbox='True',  # or False by default
+    max_height=300
+)
+
+
+#st.write('## Items Selection list')
+#grid_table = AgGrid(df, height=400, gridOptions=gridoptions, width = '100%', update_mode=GridUpdateMode.SELECTION_CHANGED, fit_columns_on_grid_load=True)
 
 st.write('## Final Items list')
 selected_row = grid_table["selected_rows"]
@@ -36,9 +71,34 @@ for i in range(len(selected_row)):
                 selected_row[i][key] = value
 
 
-st.dataframe(selected_row)
+#st.dataframe(selected_row)
 
 final_df = pd.DataFrame(selected_row)
+
+try:
+    final_df.insert(loc = 2, column = "Metal RR", value = 100)
+    final_df.insert(loc = 3, column = "Min", value = None)
+    final_df.insert(loc = 4, column = "Max", value = None)
+except:
+    pass
+
+st.dataframe(final_df)
+
+ino_qty = False
+try:
+    if "LADLE"in final_df['Type'].values:
+        ino_qty = st.text_input("Inoculant qty")
+except:
+    pass
+
+if not ino_qty:
+    st.stop()
+
+if ino_qty:
+    print(ino_qty)
+    final_df.loc[final_df['Type'] == 'LADLE', 'Min'] = ino_qty
+    final_df.loc[final_df['Type'] == 'LADLE', 'Max'] = ino_qty
+
 
 st.download_button(
    label = "Download Items CSV",
